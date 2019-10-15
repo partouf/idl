66015c14e3ee92e6db3394f0ee4538805ecb579e
Partouf <partouf@gmail.com>
2019-10-16 00:14:03 +0200

    travis test

--- delphi2csharp/test.sh
+++ delphi2csharp/test.sh
@@ -1,4 +1,4 @@
-@echo off
+#!/bin/sh
 
 cd test1
 
@@ -7,12 +7,5 @@ python ../idl2pasdllhandler.py test1.idl
 python ../idl2pasimpl2cdll.py test1.idl
 
 python ../idl2csintf.py test1.idl
-
-
-del *.exe
-dcc32 -Q -CC -B -NSSystem;WinAPI test1.dpr
-echo ----
-del *.dcu
-test1.exe
 
 cd ..
