c169d3a8f9e72c8266f7ec6372484a7b028259a2
Partouf <partouf@gmail.com>
2020-04-06 01:38:27 +0200

    unity changes

--- delphi2csharp/shared/IDLUtils.cs
+++ delphi2csharp/shared/IDLUtils.cs
@@ -4,6 +4,16 @@ namespace IDLInterop
 
     public static class InteropContainer
     {
-        public static IUnityContainer Default = new UnityContainer();
+        private static IUnityContainer Default = new UnityContainer();
+
+        public static void RegisterType<TFrom, TTo>() where TTo : TFrom
+        {
+            UnityContainerExtensions.RegisterType<TFrom, TTo>(Default);
+        }
+
+        public static T Resolve<T>()
+        {
+            return UnityContainerExtensions.Resolve<T>(Default);
+        }
     }
 }
